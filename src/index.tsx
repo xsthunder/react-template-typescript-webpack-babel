6925cedf3c494be4993a37411cf31249e965d4e0
xsthunder <xsthunder@outlook.com>
2018-10-14 21:51:31 +0800

    (config) babel object spread

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,6 @@ import ReactDom from 'react-dom';
 import App from './App';
 const ele = document.getElementById('app');
 ReactDom.render(<App></App>,ele)
-console.log('hello hot');
+const obj = {a:1};
+const a = {...obj};
+console.log('hello hot', a);
